96692bb7d2c6a7eb0193a11e2d31c04ee11e3e47
aijazahmadwani <aijazahmad9864@gmail.com>
2021-01-02 10:22:40 +0530

    almost completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -84,7 +84,7 @@ function updateHomepageForecast() {
   for(let i=0;i<3;i++){
     hours.innerHTML += `
     <tr>
-    <th style="font-size:12px;">${convertTimestamp(data.hourly[i].dt)}</th>
+    <th style="font-size:12px;">${convertTimestamp(data.hourly[i].dt,'time')}</th>
     <td>${spitOutCelcius(data.hourly[i].temp)}&deg;C</td>
     <td><img src="./assets/animated_icons/${data.hourly[i].weather[0].icon}.svg" alt="" width="100px" height="60px" class="table-icon-container"/></td>
     <td>${capitalizeFirstLetter(data.hourly[i].weather[0].description)}</td>
