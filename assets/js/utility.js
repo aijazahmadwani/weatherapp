96751e59cd3cd63d684e486a276cdbc3aa1a45bc
aijazahmadwani <aijazahmad9864@gmail.com>
2021-01-03 16:57:38 +0530

    modified code a bit

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -6,23 +6,15 @@ const spitOutCelcius = (kelvin) => {
   
 //function to convert unix timestamp to local time and date
 function convertTimestamp(timestamp, arg) {
-    var d = new Date(timestamp * 1000), // Convert the passed timestamp to milliseconds
+    let d = new Date(timestamp * 1000), // Convert the passed timestamp to milliseconds
       yyyy = d.getFullYear(),
       mm = ('0' + (d.getMonth() + 1)).slice(-2),  // Months are zero based. Add leading 0.
       dd = ('0' + d.getDate()).slice(-2),         // Add leading 0.
       hh = d.getHours(),
       h = hh,
       min = ('0' + d.getMinutes()).slice(-2),     // Add leading 0.
-      ampm = 'AM',
-      time;
-    let weekday = new Array(7);
-    weekday[0] = "Sun";
-    weekday[1] = "Mon";
-    weekday[2] = "Tue";
-    weekday[3] = "Wed";
-    weekday[4] = "Thur";
-    weekday[5] = "Fri";
-    weekday[6] = "Sat";
+      ampm = 'AM';
+    let weekday = new Array("Sun","Mon","Tue","Wed","Thur","Fri","Sat");
     day = weekday[d.getDay()];
     if (hh > 12) {
       h = hh - 12;
@@ -35,8 +27,8 @@ function convertTimestamp(timestamp, arg) {
     }
   
     //time = h + ':' + min + ' ' + ampm + ',' + dd + '-' + mm + '-' + yyyy;
-    time = h + ':' + min + ' ' + ampm;
-    date = day + ' ' + dd + '-' + mm;
+    let time = h + ':' + min + ' ' + ampm;
+    let date = day + ' ' + dd + '-' + mm;
     if (arg === 'time') {
       return time;
     }
