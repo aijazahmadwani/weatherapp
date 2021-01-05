1b7ef8d83b23673e489c231fa16ce01cc32a1b62
aijazahmadwani <aijazahmad9864@gmail.com>
2021-01-05 09:31:09 +0530

    pwa

---
+++ sw.js
@@ -0,0 +1,30 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+workbox.googleAnalytics.initialize();
+workbox.routing.registerRoute(
+  /assets/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-files',
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        purgeOnQuotaError: true,
+        maxEntries: 100
+
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  "/",
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'homepage',
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        purgeOnQuotaError: true,
+        maxEntries: 100
+
+      })
+    ]
+  })
+)
